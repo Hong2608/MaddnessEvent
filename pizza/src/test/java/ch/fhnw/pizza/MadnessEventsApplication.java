a4151891cd2017df9c2efdaa2520bd4bcc1f8ce7
Nicson17 <kalaichelvi.nicson@students.fhnw.ch>
2026-03-26 15:21:31 +0100

    Update MadnessEventsApplication.java

--- pizza/src/test/java/ch/fhnw/pizza/MadnessEventsApplication.java
+++ pizza/src/test/java/ch/fhnw/pizza/MadnessEventsApplication.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class MadnesseventApplicationTests {
+class MadnessEventsApplicationTests {
 
 	@Test
 	void contextLoads() {
