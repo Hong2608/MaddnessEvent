f443eb814d0712c8dd03a413df31c45f85448ba5
Hong2608 <holie2608@gmail.com>
2026-04-08 12:46:31 +0000

    Restore all files

---
+++ pizza/src/main/java/ch/fhnw/pizza/MadnessEventsApplication.java
@@ -0,0 +1,44 @@
+package ch.fhnw.madnessevents;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import ch.fhnw.madnessevents.business.service.MenuService;
+import ch.fhnw.madnessevents.data.domain.Pizza;
+import io.swagger.v3.oas.annotations.Hidden;
+import jakarta.annotation.PostConstruct;
+
+@SpringBootApplication
+@RestController
+@Hidden // Hide this controller from the Swagger UI
+public class MadnessEventsApplication{
+
+	@Autowired
+	private MenuService menuService;
+
+	public static void main(String[] args) {
+		SpringApplication.run(MadnessEventsApplication.class, args);
+	}
+	
+
+	// Use this method to initialize placeholder data without using Postman
+	// If you are persisting data in a file (see application.properties), initializing data that already exists will cause an error during starting the application
+	// To resolve the error, delete the file and restart the application
+	@PostConstruct
+	private void initPlaceholderData() throws Exception {
+		Pizza pizza = new Pizza();
+		pizza.setPizzaName("Margherita");
+		pizza.setPizzaToppings("Tomato sauce, mozzarella, basil");
+		menuService.addPizza(pizza);
+
+		pizza = new Pizza();
+		pizza.setPizzaName("Funghi");
+		pizza.setPizzaToppings("Tomato sauce, mozzarella, mushrooms");
+		menuService.addPizza(pizza);
+		
+	}
+
+}
---
+++ pizza/src/test/java/ch/fhnw/pizza/MadnessEventsApplication.java
@@ -0,0 +1,13 @@
+package ch.fhnw.madnessevents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MadnessEventsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
