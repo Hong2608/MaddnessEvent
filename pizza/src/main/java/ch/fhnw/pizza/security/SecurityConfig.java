c6477893c17058c3028424a94115d51b91fbeccc
Nicson17 <kalaichelvi.nicson@students.fhnw.ch>
2026-04-12 14:15:11 +0200

    Update SecurityConfig.java

--- pizza/src/main/java/ch/fhnw/pizza/security/SecurityConfig.java
+++ pizza/src/main/java/ch/fhnw/pizza/security/SecurityConfig.java
@@ -1,4 +1,4 @@
-package ch.fhnw.maddnessevent.security;
+package ch.fhnw.madnessevents.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -38,27 +38,28 @@ public class SecurityConfig {
 
     }
 
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http
-                .csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests( auth -> auth
-                        .requestMatchers("/menu").hasRole("USER") //note that the role need not be prefixed with "ROLE_"
-                        .requestMatchers("/menu/pizzas/**").hasRole("ADMIN") //note that the role need not be prefixed with "ROLE_"
-                        .requestMatchers("/menu/**",
-                                                    "/**", //allow access to the home page
-                                                    "/swagger-ui.html", //allow access to the swagger UI
-                                                    "/v3/api-docs/**",  //allow access to the swagger API documentation
-                                                    "/swagger-ui/**",   //allow access to the swagger UI
-                                                    "/h2-console/**")   //allow access to the h2-console
-                                                    .permitAll() 
-                        .anyRequest().hasAuthority("SCOPE_READ")           
-                )       
-                .headers(headers -> headers.frameOptions(frameOptions -> frameOptions.disable())) //needed to allow access to the h2-console
-                .formLogin(withDefaults()) //need to include a static import for withDefaults, see the imports at the top of the file
-                .httpBasic(withDefaults())
-                .build(); 
-    } 
+   @Bean
+public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    return http
+            .csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(auth -> auth
+                    .requestMatchers(
+                            "/",
+                            "/events",
+                            "/swagger-ui.html",
+                            "/v3/api-docs/**",
+                            "/swagger-ui/**",
+                            "/h2-console/**"
+                    ).permitAll()
+                    .requestMatchers("/menu").hasRole("USER")
+                    .requestMatchers("/menu/pizzas/**").hasRole("ADMIN")
+                    .anyRequest().authenticated()
+            )
+            .headers(headers -> headers.frameOptions(frameOptions -> frameOptions.disable()))
+            .formLogin(withDefaults())
+            .httpBasic(withDefaults())
+            .build();
+}
 
 
 
