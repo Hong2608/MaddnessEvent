7e32d68fc3cbcf7893240be2f632fc12b4f4a3ce
Nicson17 <kalaichelvi.nicson@students.fhnw.ch>
2026-04-12 12:22:55 +0200

    Update EventController.java

--- pizza/src/main/java/ch/fhnw/pizza/controller/EventController.java
+++ pizza/src/main/java/ch/fhnw/pizza/controller/EventController.java
@@ -1,1 +1,14 @@
 
+package ch.fhnw.madnessevents.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EventController {
+
+    @GetMapping("/events")
+    public String getEvents() {
+        return "Events API is working!";
+    }
+}
