e9944598bc5fa0a48e4450863d79d8e3b2ba959e
Ajmal-shafaq <ajmal.shafaq@gmail.com>
2026-05-20 13:08:55 +0000

    Remove old duplicate project folders

--- MadnessEvent-boot/src/main/java/ch/fhnw/madnessevent/data/domain/ProductCategory.java
+++
@@ -1,6 +0,0 @@
-package ch.fhnw.madnessevent.data.domain;
-
-public enum ProductCategory {
-    APPAREL,       // T-shirt, hoodie, hat, headwears, tank-top
-    ACCESSORIES    // Branded hat, wristband, sticker, lighter
-}
